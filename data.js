5a6a4efdb2160222e6d222f35ba85a1dfffed6b2
Piotr Wiliński <piotrwilinski@MacBook-Air-Piotr.local>
2020-11-28 20:37:39 +0100

    Created structure of the project

---
+++ data.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+  return {
+    projects: [
+      {
+        id: 1, name: "IP Address Tracker", description: "IP Address Tracker", techs: ["JavaScript"], img: "/", url: "/"
+      }
+    ]
+  }
+}
